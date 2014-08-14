36603dbb263e2173f1d1aee1406b8cb873d89caa
UtkinG <utking@mail.ru>
2014-08-14 11:24:50 +1100

    added fetching all messages for category and subcategories

--- app/models/UserMessage.php
+++ app/models/UserMessage.php
@@ -11,5 +11,34 @@ class UserMessage extends Phalcon\Mvc\Model
             'alias' => 'Receiver'
         ));
     }
+
+    protected static function di()
+    {
+        return \Phalcon\DI\FactoryDefault::getDefault();
+    }
+    
+    protected static function getBuilder()
+    {
+        $di      = self::di();
+        $manager = $di['modelsManager'];
+        $builder = $manager->createBuilder();
+        $builder->from(get_called_class());
+
+        return $builder;
+    }
+
+	public static function findMessages($category_id, $is_new = false) {
+		$cat_ids = Categories::getChildIDs($category->id);
+		$cat_ids[] = $category_id;
+        $messages = self::getBuilder()
+                ->from('UserMessage')
+				->inWhere("category_id", $cat_ids)
+				->andWhere('to_user_id = 0');
+		if ($is_new) {
+			$messages->andWhere('is_new = 1');
+		}
+		return $messages->getQuery()->execute();
+	}
+
     
 }
